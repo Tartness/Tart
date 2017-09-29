dc4ffb73c395162718c397abdd92e83c901aa31d
Nicolas Degen <nicolas@kapanu.com>
2017-09-29 17:41:30 +0200

    added include all cmake file

---
+++ Modules/tart-include.cmake
@@ -0,0 +1,4 @@
+# Include Tart Modules
+INCLUDE("${CMAKE_CURRENT_LIST_FILE}/../yaml-parser.cmake")
+INCLUDE("${CMAKE_CURRENT_LIST_FILE}/../configure-target.cmake")
+INCLUDE("${CMAKE_CURRENT_LIST_FILE}/../git-tools.cmake")
