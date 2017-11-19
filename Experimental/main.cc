6a023e5f73e6e3dbf75d0d766c00e16e22a0f476
Nicolas Degen <ndegen@me.com>
2017-11-19 20:18:20 +0100

    implemented opengl preinstalled configuration

--- Experimental/main.cc
+++ Experimental/main.cc
@@ -1,8 +1,15 @@
 #include <iostream>
 
+#include <OpenGL/gl.h>
+#include <OpenGL/glu.h>
+#include <GLUT/glut.h>
+
 int main(int argc, char **argv) {
   
-  class_a.printCounter();
-  std::cout << "Hello world!" << std::endl;
+  glutInit(&argc, argv);
+  glutCreateWindow("GLUT");
+  
+//  glewInit();
+  printf("OpenGL version supported by this platform (%s): \n", glGetString(GL_VERSION));
   return 1;
 }
