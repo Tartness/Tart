608bc2b0cf62b133a43abaf0d8dd44e2aed12f6c
Nicolas Degen <ndegen@me.com>
2017-11-25 20:53:27 +0100

    added compile definition configuration

--- Modules/tart-definitions.cmake
+++ Modules/tart-definitions.cmake
@@ -1,6 +1,7 @@
 
 # TART_CURRENT_WORKSPACE_DIR sets the path where all packages are searched and cloned to
-SET(TART_CURRENT_WORKSPACE_DIR "${CMAKE_CURRENT_SOURCE_DIR}/..")
+
+GET_FILENAME_COMPONENT(TART_CURRENT_WORKSPACE_DIR "${CMAKE_CURRENT_SOURCE_DIR}/.." REALPATH)
 
 # TART_TARGET_OS will efine the current OS
 SET(TART_TARGET_OS "UNDEF" CACHE STRING "Set current OS Tart identifier")
