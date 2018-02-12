c79e6074b2f7f0611e79ea79f9fcf6e006054de6
Nicolas Degen <nicolas@kapanu.com>
2018-02-12 15:10:21 +0100

    fixes for system variables

--- Modules/System/tart-definitions.cmake
+++ Modules/System/tart-definitions.cmake
@@ -2,9 +2,3 @@
 # TART_CURRENT_WORKSPACE_DIR sets the path where all packages are searched and cloned to
 
 GET_FILENAME_COMPONENT(TART_CURRENT_WORKSPACE_DIR "${CMAKE_CURRENT_SOURCE_DIR}/.." REALPATH)
-
-# TART_TARGET_OS will efine the current OS
-SET(TART_TARGET_OS "UNDEF" CACHE STRING "Set current OS Tart identifier")
-
-# TART_TARGET_ARCH will efine the current OS
-SET(TART_TARGET_ARCH "UNDEF" CACHE STRING "Set current Architecture Tart identifier")
