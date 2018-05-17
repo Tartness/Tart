9fc9e1da13e007b7013d17f4a12b608904ca5650
Nicolas Degen <nicolas@kapanu.com>
2018-05-17 10:10:28 +0200

    bugfix for subtree getters, logging

--- Scripts/tlog.sh
+++ Scripts/tlog.sh
@@ -1,35 +1,33 @@
 #!/bin/bash -e
 
-RESTORE=$(echo -en '\033[0m')
-RED=$(echo -en '\033[00;31m')
-GREEN=$(echo -en '\033[00;32m')
-YELLOW=$(echo -en '\033[00;33m')
-BLUE=$(echo -en '\033[00;34m')
-MAGENTA=$(echo -en '\033[00;35m')
-PURPLE=$(echo -en '\033[00;35m')
-CYAN=$(echo -en '\033[00;36m')
-LIGHTGRAY=$(echo -en '\033[00;37m')
-LRED=$(echo -en '\033[01;31m')
-LGREEN=$(echo -en '\033[01;32m')
-LYELLOW=$(echo -en '\033[01;33m')
-LBLUE=$(echo -en '\033[01;34m')
-LMAGENTA=$(echo -en '\033[01;35m')
-LPURPLE=$(echo -en '\033[01;35m')
-LCYAN=$(echo -en '\033[01;36m')
-WHITE=$(echo -en '\033[01;37m')
+RESTORE=$(echo '\033[0m')
+RED=$(echo '\033[00;31m')
+GREEN=$(echo '\033[00;32m')
+YELLOW=$(echo '\033[00;33m')
+BLUE=$(echo '\033[00;34m')
+MAGENTA=$(echo '\033[00;35m')
+PURPLE=$(echo '\033[00;35m')
+CYAN=$(echo '\033[00;36m')
+LIGHTGRAY=$(echo '\033[00;37m')
+LRED=$(echo '\033[01;31m')
+LGREEN=$(echo '\033[01;32m')
+LYELLOW=$(echo '\033[01;33m')
+LBLUE=$(echo '\033[01;34m')
+LMAGENTA=$(echo '\033[01;35m')
+LPURPLE=$(echo '\033[01;35m')
+LCYAN=$(echo '\033[01;36m')
+WHITE=$(echo '\033[01;37m')
 
 function tlogTemp {
-  if [[ false ]]; then
-    return
+  if [[ false = true ]]; then
+    echo "${LGREEN}$1${RESTORE}"
   fi
-  echo "${LGREEN}$1${RESTORE}"
 }
 
 function tlogDebug {
-  if [[ true ]]; then
-    return
+  if [[ false = true ]]; then
+    echo "${CYAN}$1${RESTORE}"
   fi
-  echo "${CYAN}$1${RESTORE}"
 }
 
 function tlogInfo {
